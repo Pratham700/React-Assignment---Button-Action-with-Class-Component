a981847275f2d03dc782454698996b5f0b054000
Pratham700 <prathamsinghal.singhal@gmail.com>
2020-11-17 17:48:14 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,25 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
-		this.state = { visibility : false} ;
+		this.state = { 
+			visibility : false
+		} ;
 	};
 
-	showPara = () => {
-		this.setState({visibility:true}) ;
-	}
+	showPara() {
+		this.setState(
+			{
+				visibility:true
+			}) ;
+	} 
 
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
 				<button id = "click" onClick = {this.showPara}> Show Paragraph </button>
-				this.state.visibility ? (<p id = "para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> ): null ;
-				
+				{this.state.visibility ? (<p id = "para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> ): null} ;
+	
     		</div>
     	);
     }
